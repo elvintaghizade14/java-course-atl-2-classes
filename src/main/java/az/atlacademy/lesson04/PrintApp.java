929666f11047827e7a10e12d28ad331724396da8
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-03-03 19:45:52 +0400

    print examples attached

---
+++ src/main/java/az/atlacademy/lesson04/PrintApp.java
@@ -0,0 +1,14 @@
+package az.atlacademy.lesson04;
+
+public class PrintApp {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, World");
+        System.out.println("Hello, World");
+        System.out.printf("Hello, %s", "World");
+        System.out.printf("Hello, %s\n", "World");
+        System.out.printf("\tHello, World: %.8f", 50.044);
+
+    }
+
+}
