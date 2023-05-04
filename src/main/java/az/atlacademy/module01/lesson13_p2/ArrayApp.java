b46f5797a0615131312704ca3cab49f747ede6b0
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-05-04 20:45:44 +0400

    lesson13 p2 class code attached

---
+++ src/main/java/az/atlacademy/module01/lesson13_p2/ArrayApp.java
@@ -0,0 +1,19 @@
+package az.atlacademy.module01.lesson13_p2;
+
+import java.util.Arrays;
+
+public class ArrayApp {
+
+    public static void main(String[] args) {
+        int[] nums = {0, -1, 12, 3, 9};
+        System.out.println(Arrays.toString(nums));
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+        String[] words = {"B", "S", "E", "T", "A"};
+        System.out.println(Arrays.toString(words));
+        Arrays.sort(words);
+        System.out.println(Arrays.toString(words));
+    }
+
+}
