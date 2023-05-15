f98b96662eda91cfcfcaa28094fa51a4d48657fa
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-05-15 23:52:09 +0400

    lesson16 p3 class code attached

---
+++ src/main/java/az/atlacademy/module01/lesson16_p3/StackApp.java
@@ -0,0 +1,12 @@
+package az.atlacademy.module01.lesson16_p3;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class StackApp {
+
+    public static void main(String[] args) {
+        Deque<String> words = new LinkedList<>();
+    }
+
+}
