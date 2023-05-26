0c56ef55b36ab413a31fcf1ca75ef191a870647a
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-05-26 20:38:47 +0400

    lesson18 p3 class code attached

---
+++ src/main/java/az/atlacademy/module01/lesson18_p3/JsonApp.java
@@ -0,0 +1,45 @@
+package az.atlacademy.module01.lesson18_p3;
+
+import az.atlacademy.module01.lesson18_p2.Student;
+import com.fasterxml.jackson.xml.XmlMapper;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class JsonApp {
+
+    public static final String RESOURCE = "src/main/java/az/atlacademy/module01/lesson18_p3/resource/";
+
+
+    public static void main(String[] args) {
+        Student elvin = new Student(24, 93, "Elvin");
+
+        try {
+            XmlMapper xmlMapper = new XmlMapper();
+            String elvinJson = xmlMapper.writeValueAsString(elvin);
+            System.out.println(elvinJson);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String elvinJson = objectMapper.writeValueAsString(elvin);
+            System.out.println(elvinJson);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(
+                new BufferedOutputStream(new FileOutputStream(RESOURCE + "students.ser")))) {
+            byte[] bytes = new ObjectMapper().writeValueAsBytes(elvin);
+            oos.write(bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
