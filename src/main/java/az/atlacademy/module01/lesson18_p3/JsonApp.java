3e17b73645fdd56a6cde19852b70147f65d2dbe6
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-05-26 20:46:34 +0400

    lesson18 p3 class code attached

--- src/main/java/az/atlacademy/module01/lesson18_p3/JsonApp.java
+++ src/main/java/az/atlacademy/module01/lesson18_p3/JsonApp.java
@@ -2,10 +2,16 @@ package az.atlacademy.module01.lesson18_p3;
 
 import az.atlacademy.module01.lesson18_p2.Student;
 import com.fasterxml.jackson.xml.XmlMapper;
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class JsonApp {
@@ -38,6 +44,34 @@ public class JsonApp {
             oos.write(bytes);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        List<Integer> randomNums = new Random()
+                .ints(10, 0, 100)
+                .boxed()
+                .collect(Collectors.toList());
+
+        try {
+            BufferedOutputStream bos =
+                    new BufferedOutputStream(new FileOutputStream(RESOURCE + "nums.ser"));
+            byte[] bytes = new byte[10];
+            for (int i = 0; i < randomNums.size(); i++) {
+                bytes[i] = randomNums.get(i).byteValue();
+            }
+            bos.write(bytes);
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedInputStream bis =
+                    new BufferedInputStream(new FileInputStream(RESOURCE + "nums.ser"));
+            byte[] bytes = bis.readAllBytes();
+            System.out.println(Arrays.toString(bytes));
+            bis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
