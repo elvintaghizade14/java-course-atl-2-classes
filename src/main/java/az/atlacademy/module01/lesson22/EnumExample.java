72861356d7948bdf45f7f2832ba66d4d760cc3f1
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-06-02 20:32:49 +0400

    lesson22 class code attached

---
+++ src/main/java/az/atlacademy/module01/lesson22/EnumExample.java
@@ -0,0 +1,9 @@
+package az.atlacademy.module01.lesson22;
+
+public class EnumExample {
+
+    public static void main(String[] args) {
+        System.out.println(OrderStatus.DELIVERED.name());
+    }
+
+}
---
+++ src/main/java/az/atlacademy/module01/lesson22/OrderStatus.java
@@ -0,0 +1,7 @@
+package az.atlacademy.module01.lesson22;
+
+public enum OrderStatus {
+
+    ACCEPTED, REJECTED, IN_PROGRESS, DELIVERED
+
+}
