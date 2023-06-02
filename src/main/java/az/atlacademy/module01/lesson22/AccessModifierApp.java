d333b45cffc9ae5888a31927fd54a0e57c931bb8
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-06-02 20:26:11 +0400

    lesson22 class code attached

--- src/main/java/az/atlacademy/module01/lesson22/AccessModifierApp.java
+++ src/main/java/az/atlacademy/module01/lesson22/AccessModifierApp.java
@@ -3,6 +3,8 @@ package az.atlacademy.module01.lesson22;
 public class AccessModifierApp {
 
     public static void main(String[] args) {
+        String a = "a";
+        System.out.println(a);
         System.out.println("AccessModifierApp");
     }
 
