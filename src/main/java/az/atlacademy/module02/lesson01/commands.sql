71cefcb9d202b0a50ab744b1c1bccea44f0fd379
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-06-19 20:40:41 +0400

    lesson01 class code attached

---
+++ src/main/java/az/atlacademy/module02/lesson01/commands.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE jbed_test_0;
+
+DROP DATABASE jbed_test_0;
+
+CREATE TABLE Persons
+(
+    PersonID  int,
+    LastName  varchar(255),
+    FirstName varchar(255),
+    Address   varchar(255),
+    City      varchar(255)
+);
+
+DROP TABLE Persons;
