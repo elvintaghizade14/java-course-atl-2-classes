b4aaec2373d2eb801161450260eebb42c4fb4871
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-07-06 20:51:08 +0400

    lesson04 class code attached

--- src/main/java/az/atlacademy/module02/lesson04/lesson.sql
+++ src/main/java/az/atlacademy/module02/lesson04/lesson.sql
@@ -16,3 +16,32 @@ alter table public.students
 
 create unique index if not exists students_pin_uindex
     on public.students (pin);
+
+
+create table if not exists public.phone_numbers
+(
+    id           serial
+        constraint phone_numbers_pk
+            primary key,
+    phone_number varchar(10) not null
+);
+
+create table if not exists public.students
+(
+    id              serial
+        constraint students_pk
+            primary key,
+    name            varchar(255)               not null,
+    created_date    timestamp(3) default now(),
+    active          boolean      default false not null,
+    pin             varchar                    not null
+        constraint pin_check
+            check (length((pin)::text) = ANY (ARRAY [5, 7])),
+    phone_number_id integer
+        constraint students_phone_numbers_id_fk
+            references public.phone_numbers
+);
+
+create unique index if not exists students_pin_uindex
+    on public.students (pin);
+
