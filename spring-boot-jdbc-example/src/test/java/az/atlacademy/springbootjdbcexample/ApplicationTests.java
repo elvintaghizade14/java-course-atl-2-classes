f3139d3436a729d57f8b128082c7abe184f4e9e7
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-08-10 19:42:23 +0400

    lesson04 class code attached

---
+++ spring-boot-jdbc-example/src/test/java/az/atlacademy/springbootjdbcexample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atlacademy.springbootjdbcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
