4628e7d2712416063891c26a94270934a52206dc
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-08-21 21:00:26 +0400

    lesson07 class code attached

---
+++ spring-product-management-app/src/test/java/az/atl/productmanagementapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atl.productmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
